be03ef495bee56e61f5209bb58616b5005877adf
yuliia-petrenko <142539128+yuliia-petrenko@users.noreply.github.com>
2024-01-14 17:26:55 +0000

    Deploying to gh-pages from @ yuliia-petrenko/goit-js-hw-10@ecc01fed27bfac128e2a6f94c84ebe821a5f7dff 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-651d7991.js";const e=document.querySelector(".form-snack");e.addEventListener("submit",t=>{e.delay.value,e.state.value});
+//# sourceMappingURL=commonHelpers2.js.map
